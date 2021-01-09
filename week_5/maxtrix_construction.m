eea2c17f6cf32a5f7d70d9b72af2c9df00f4a66a
Chris Y <KXYang@users.noreply.github.com>
2021-01-09 13:54:46 -0500

    submission of assignment: Matrix Construction

---
+++ week_5/maxtrix_construction.m
@@ -0,0 +1,9 @@
+%Matrix Construction
+function T = trio(n, m)
+    T = randi(10, 3*n, m);
+    T(1:n, :) = 1;
+    T(n+1:2*n, :) = 2;
+    T(2*n+1:end, :) = 3;
+ 
+ %Code to call your function
+ T = trio(2,4)
