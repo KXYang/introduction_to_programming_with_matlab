accc8b2deb9a2dbc5839ac3c51fc0d1214c5ed67
Chris Y <KXYang@users.noreply.github.com>
2021-01-10 14:04:27 -0500

    submission of assignment: If-statement practice

---
+++ week_6/if_statements.m
@@ -0,0 +1,12 @@
+%Practice if-statements
+%Function
+function out = picker(condition, in1, in2)
+  if condition == 1
+  out = in1;
+  else 
+  out = in2;
+end
+
+%Code to call your function
+out = picker(true,1,2)
+out = picker(false,1,2)
